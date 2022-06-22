51e0a0e117b022b87bc142ef7ebe61930a86a8b6
Jade-abr <jade@finalgo.fr>
2022-06-22 17:53:18 +0200

    in progress

--- mon-app/src/index.js
+++ mon-app/src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
+/*  <button className="square" onClick={function() { alert('clic'); }}> */
+/*  <button className="square" onClick={() => alert('clic')}> */
+
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
